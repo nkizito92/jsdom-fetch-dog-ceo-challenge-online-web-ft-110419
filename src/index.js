1e44688eccab0a16c1579e4d10829462be5c9edd
Kizito Njoku <nkizito57@gmail.com>
2020-03-06 05:01:41 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 console.log('%c HI', 'color: firebrick')
-
-
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = 'https://dog.ceo/api/breeds/list/all';
 
 function fetchImage() {
   return fetch(imgUrl)
   .then(res => res.json())
-  .then( json => renderImages(json))
+  .then( json => renderImages(json));
+  // .then( json => console.log(json));
  }
 
 function renderImages(json) {
   const images = document.querySelector("#dog-image-container");
-  json.forEach(dogs => {
+  json.message.forEach(dogs => {
   const img = document.createElement("img")
-    images.innerHTML = `<img src="${dogs.message}"/>`;
-    img.appendChild(images);
+    img.src =dogs;
+    images.appendChild(img);
   }); 
 }
 
